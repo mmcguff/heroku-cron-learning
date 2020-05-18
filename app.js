289f286b481f45157651d6df3ba4be7d73108c02
Matthew McGuff <mmcguff@byui.edu>
2020-05-18 16:33:02 -0600

    changed for heroku scheduler.

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-const cron = require('node-cron');
-const express = require('express');
 
-app = express();
+function start(){
+    console.log('Cron job ran...');
+}
 
-const every3Seconds = '*/3 * * * * *';
-const every10Seconds = '*/10 * * * * *';
-const every15Mintues = '*/15 * * * *';
-const every30MIntues = '*/30 * * * *';
-const everyHour = '* */1 * * *';
 
-const task = cron.schedule(every10Seconds, () => {
-    console.log("Cron Job executed");
-});
+start();
 
-task.start();
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
